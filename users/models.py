64a7f09d4a5db7feb2ccd68d018ce8b90d4c775b
JVacca12 <johan.vacca2@gmail.com>
2021-09-25 19:21:31 -0500

    primer migrate

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField('correo electrónico', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    modified = models.DateTimeField(auto_now=True)
+    phone = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    is_recruiter = models.BooleanField(default=False)
