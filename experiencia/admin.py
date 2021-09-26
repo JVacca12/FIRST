1d7987be8093ee6ee8c61f6f0152eed0a803587d
JVacca12 <johan.vacca2@gmail.com>
2021-09-25 20:43:38 -0500

    Añadida App Experiencia

---
+++ experiencia/admin.py
@@ -0,0 +1,6 @@
+# Django
+from django.contrib import admin
+from experiencia.models import Experiencia
+
+# Models
+admin.site.register(Experiencia)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
 # Register your models here.
+"""User admin classes."""
+
+# Django
+from django.contrib import admin
+
+# Models
+from users.models import User
+from experiencia.models import Experiencia
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """User admin."""
+
+    list_display = ('pk', 'username', 'email','first_name','last_name') #Columnas de la tabla visibles, first and last name son campos intrínsecos de user
+    list_display_links = ('pk', 'username', 'email','first_name','last_name') #Cliqueables
+
+    search_fields = (
+        'email',
+        'username',
+        'first_name',
+        'last_name',
+    )
+
+    list_filter = (
+        'is_active',
+        'is_staff',
+        'date_joined',
+        'modified',
+    )
+
+    readonly_fields = ('date_joined', 'modified',)
